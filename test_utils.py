38bba2858cc3bb2306de4865d03f55744fbc927c
Ubuntu <ubuntu@ip-172-31-34-216.ec2.internal>
2021-11-08 23:50:29 +0000

    init files

---
+++ test_utils.py
@@ -0,0 +1,6 @@
+from utils import plot_line
+
+
+def test_plot_line():
+    data = [1, 2, 3]
+    plot_line(x=data, y=data)
---
+++ utils.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+
+def plot_line(x, y):
+    plt.plot(x, y)
+    plt.show()
+    # plt.savefig('line.png', bbox_inches='tight')
